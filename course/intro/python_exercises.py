3bc8fa43c3e2f533e24cee26a817f03b90d86961
kadeknova <ik376@exeter.ac.uk>
2025-11-17 10:01:54 +0000

    filled blank python exercise

---
+++ course/intro/python_exercises.py
@@ -0,0 +1,45 @@
+# course/intro/python_exercises.py
+import datetime
+
+
+def parse_date(date_string):
+    return datetime.datetime.strptime(date_string, "%Y-%m-%d").date()
+
+
+def sum_list(numbers):
+    return sum(numbers)
+
+
+def max_value(lst):
+    return max(lst)
+
+
+def reverse_string(s):
+    return s[::-1]
+
+
+def filter_even(lst):
+    return [x for x in lst if x % 2 == 0]
+
+
+def get_fifth_row(df):
+    return df.iloc[4]
+
+
+def column_mean(df, column):
+    return df[column].mean()
+
+
+def lookup_key(d, key):
+    return d.get(key)
+
+
+def count_occurrences(lst):
+    counts = {}
+    for item in lst:
+        counts[item] = counts.get(item, 0) + 1
+    return counts
+
+
+def list_to_string(lst):
+    return ','.join(map(str, lst))

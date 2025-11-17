9c71a157f90068d4d338c4663c60c537f24dd9c7
kadeknova <ik376@exeter.ac.uk>
2025-11-17 10:01:01 +0000

    correcting hello.py

--- course/intro/hello.py
+++ course/intro/hello.py
@@ -2,6 +2,7 @@ def greet():
     """Write a function that PRINTS 'Hello, World!'"""
     print("Hello, World!")
 
+
 def add_two_numbers(a, b):
     """Write a function that adds the numbers a and b"""
     return a+b

1cfb4b3de9dea37c8b994fd42ef6ca2ce873c61c
kadeknova <ik376@exeter.ac.uk>
2025-11-17 10:02:21 +0000

    change return 0 to plot figure

--- course/regression/eda.py
+++ course/regression/eda.py
@@ -10,7 +10,8 @@ def _boxplot(df, x_var, y_var, title):
     """Given a data frame 'df' containing categorical variable 'x_var'
     and outcome variable 'y_var' produce a box plot of the distribution of the y_variable
     for different levels of y_var. The box plot should have title 'title'"""
-    return 0
+    fig = px.box(df, x=x_var, y=y_var, title=title)
+    return fig
 
 
 def boxplot_age():
